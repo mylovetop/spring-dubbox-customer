e62527f50a39d95bc599fc1ed45ed3a85b528b41
mylovetop <zdsoft@zdsoftmatoMac-mini-2.local>
2014-10-27 17:37:55 +0800

    spring-dubbox-customer
    消费者

---
+++ src/main/java/com/springapp/mvc/dubbox/consumer/ChatController.java
@@ -0,0 +1,48 @@
+package com.springapp.mvc.dubbox.consumer;
+
+import com.springapp.mvc.dubbox.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Created by zdsoft on 14-10-27.
+ */
+@Controller
+public class ChatController {
+
+
+    private DemoService demoService;
+
+    @RequestMapping("/")
+    public String chat(ModelMap modelMap){
+        modelMap.put("hello", "nothing");
+        if (null != demoService){
+            modelMap.put("hello", demoService.sayHello());
+        }
+
+        return "hello";
+    }
+
+
+    public void setDemoService(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"application.xml"});
+        context.start();
+
+        DemoService demoService = (DemoService) context.getBean("demoService"); //
+        String hello = demoService.sayHello(); // ִ
+        System.out.println("--我是消费者");
+        System.out.println(hello);
+
+        System.in.read();
+    }
+}
